752984f0297a9702c4dadeb80a935d109f4b6e10
Steffen Kowalski <scipper@myscipper.de>
2023-10-04 21:08:17 +0200

    initial commit with working test suite

---
+++ src/test/java/de/myscipper/AppTest.java
@@ -0,0 +1,13 @@
+package de.myscipper;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+
+    @Test
+    public void shouldDoNothing() {
+        assertTrue(true);
+    }
+}
